197a52e68fa66c321470986d5c699da8952a2ba3
Anton Malynevskyi <malina.elfg@gmail.com>
2024-05-06 22:52:09 +0200

    Somehow works

--- functions.c
+++ functions.c
@@ -11,57 +11,66 @@ void removeSpaces(char word[]) {
     word[j] = '\0';
 }
 
-//перевіряємо scalar на наявність помилок. Якщо помилку знайдено, то записуємо її індекс в indexes.
+// Перевіряє скаляр на наявність помилок. Якщо помилку знайдено,
+// додає її індекс до масиву indexes.
 int checkScalarProcess(const char scalar[], int indexes[]) {
-    static int index = 0;
-    static int counter = 0;
-    static int seen_dots = 0;
-    static int found_constant = 0;
-    static int index_of_first_symbol = 0;
+    // Статичні змінні для відстеження стану та індексів помилок
+    static int index = 0; // Поточний індекс у скалярі
+    static int counter = 0; // Кількість знайдених помилок
+    static int seen_dots = 0; // Кількість крапок (.)
+    static int found_constant = 0; // Чи знайдено константу (число)
+    static int index_of_first_symbol = 0; // Індекс першого символу після крапок (.)
+
+    // Кінець скаляра: перевірка на завершення перегляду
     if (scalar[index] == '\0') {
+        // Якщо знайдено константу без крапок, це помилка
         if (found_constant && seen_dots == 0) {
-            indexes[counter++] = 0;
-        }
-        else if(scalar[strlen(scalar)-1] == '.') {
-            index = strlen(scalar)-1;
+            indexes[counter++] = 0; // Індекс 0 для помилки на початку
+            // Якщо останній символ - крапка, переглянути всі крапки
+        } else if (scalar[strlen(scalar) - 1] == '.') {
+            index = strlen(scalar) - 1;
             while (scalar[index] == '.') {
-                indexes[counter++] = index--;
+                indexes[counter++] = index--; // Додавання індексів крапок до помилок
             }
         }
-        return counter;
+        return counter; // Повернення кількості знайдених помилок
     }
+
+    // Символ - крапка (.)
     if (scalar[index] == '.') {
-        found_constant = 0;
-        seen_dots++;
+        found_constant = 0; // Скидання прапора константи
+        seen_dots++; // Збільшення лічильника крапок
         if (seen_dots >= 2) {
-            /*if (!isalnum(scalar[index+1] )) {
-                indexes[counter++] = index;
-            }*/
-            index_of_first_symbol = index + 1;
+            index_of_first_symbol = index + 1; // Індекс першого символу після крапок
         }
     }
+
+    // Визначення константи (числа)
     if ((index == 0 || seen_dots >= 2) && isdigit(scalar[index_of_first_symbol])) {
-        found_constant = 1;
+        found_constant = 1; // Встановлення прапора константи
     }
+
+    // Перевірка помилок для різних символів
     if (index == 0 && !isalnum(scalar[index])) {
-        indexes[counter++] = index;
+        indexes[counter++] = index; // Помилка: не алфавітно-цифровий символ на початку
     } else if (!isalnum(scalar[index]) && scalar[index] != ',' && scalar[index] != '.') {
-            indexes[counter++] = index;
+        indexes[counter++] = index; // Помилка: недопустимий символ
     } else if (scalar[index] == ',' && (found_constant || seen_dots == 2)) {
-        indexes[counter++] = index;
+        indexes[counter++] = index; // Помилка: кома після константи або двох крапок
     } else if (scalar[index] == ',' && !isalpha(scalar[index + 1])) {
-        if (scalar[index + 1] != '\0' && scalar[index + 1] != '.')
-            indexes[counter++] = index + 1;
-        else {
-            indexes[counter++] = index;
+        if (scalar[index + 1] != '\0' && scalar[index + 1] != '.') {
+            indexes[counter++] = index + 1; // Помилка: кома перед не алфавітним символом
+        } else {
+            indexes[counter++] = index; // Помилка: зайва кома в кінці
         }
     } else if (seen_dots > 2 && scalar[index] == '.') {
-        indexes[counter++] = index;
+        indexes[counter++] = index; // Помилка: більше двох послідовних крапок
     } else if (scalar[index] == '.' && scalar[index + 1] != '.' && seen_dots == 1) {
-        indexes[counter++] = index;
+        indexes[counter++] = index; // Помилка: крапка після крапки (..), але не три крапки (...)
     } else if (found_constant && !isdigit(scalar[index]) && scalar[index] != ',' && scalar[index] != '.') {
-        indexes[counter++] = index;
+        indexes[counter++] = index; // Помилка: символ після константи повинен бути цифрою, комою або крапкою
     }
-    index++;
-    return checkScalarProcess(scalar, indexes);
+
+    index++; // Перейти до наступного символу в скалярі
+    return checkScalarProcess(scalar, indexes); // Рекурсивний виклик для перевірки решти скаляра
 }
