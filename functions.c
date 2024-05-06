386e0d842361ff5e8bd50b98400043ab1ec88c47
Anton Malynevskyi <malina.elfg@gmail.com>
2024-05-06 20:24:33 +0200

    Some bugs

--- functions.c
+++ functions.c
@@ -18,7 +18,7 @@ int checkScalarProcess(const char scalar[], int indexes[]) {
     static int seen_coma = 0;
     static int seen_dots = 0;
     static int found_constant = 0;
-    static int index_of_first_symbol;
+    static int index_of_first_symbol = 0;
     if (scalar[index] == '\0') {
         return counter;
     }
@@ -31,25 +31,26 @@ int checkScalarProcess(const char scalar[], int indexes[]) {
     } else if (scalar[index] == ',') {
         seen_coma = 1;
     }
+    if ((index == 0 || seen_dots == 2) && isdigit(scalar[index_of_first_symbol])) {
+        found_constant = 1;
+    }
     if (index == 0 && !isalnum(scalar[index])) {
         indexes[counter++] = index;
-    } else if ((index == 0 || seen_dots == 2) && isdigit(scalar[index_of_first_symbol])) {
-        found_constant = 1;
-    } else if (seen_coma && scalar[index] == '.') {
-        indexes[counter++] = index;
+    } else if (!isalnum(scalar[index]) && scalar[index] != '.') {
+            indexes[counter++] = index;
     } else if (scalar[index] == ',' && (found_constant || seen_dots == 2)) {
         indexes[counter++] = index;
     } else if (scalar[index] == ',' && !isalpha(scalar[index + 1])) {
-        indexes[counter++] = index+1;
+        if (scalar[index + 1] != '\0' && scalar[index + 1] != '.')
+            indexes[counter++] = index + 1;
+        else {
+            indexes[counter++] = index;
+        }
     } else if (seen_dots > 2 && scalar[index] == '.') {
         indexes[counter++] = index;
     } else if (scalar[index] == '.' && scalar[index + 1] != '.' && seen_dots == 1) {
         indexes[counter++] = index;
-    } else if (index == 1 && !isalnum(scalar[index - 1]) && scalar[index] == '.' && scalar[index + 1] == '.') {
-        indexes[counter++] = index;
     } else if (found_constant && !isdigit(scalar[index]) && scalar[index] != ',' && scalar[index] != '.') {
-        indexes[counter++] = index;
-    } else if (!isalnum(scalar[index]) && scalar[index] != ',' && scalar[index] != '.') {
         indexes[counter++] = index;
     }
     index++;
